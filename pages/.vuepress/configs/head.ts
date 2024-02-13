acbb58ad1c320f92ce550e8fe0d76f31fbf8a1fb
Oleg <vetti.ch@ya.ru>
2024-02-13 20:55:31 +0500

    fix: remove favicons

--- pages/.vuepress/configs/head.ts
+++ pages/.vuepress/configs/head.ts
@@ -1,30 +1,7 @@
 import type { HeadConfig } from '@vuepress/core'
 
 export const head: HeadConfig[] = [
-	[
-		'link',
-		{
-			rel: 'icon',
-			type: 'image/png',
-			sizes: '16x16',
-			href: `/favicon/favicon-16x16.png`,
-		},
-	],
-	[
-		'link',
-		{
-			rel: 'icon',
-			type: 'image/png',
-			sizes: '32x32',
-			href: `/favicon/favicon-32x32.png`,
-		},
-	],
-	['link', { rel: 'manifest', href: '/site.webmanifest' }],
 	['meta', { name: 'application-name', content: 'ParrotPoster docs' }],
 	['meta', { name: 'apple-mobile-web-app-title', content: 'ParrotPoster docs' }],
 	['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-	[
-		'link',
-		{ rel: 'apple-touch-icon', href: `/favicon/apple-touch-icon.png` },
-	],
 ]
